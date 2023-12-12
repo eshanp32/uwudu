e719aeda82ba833d3ffe28c4366529c210ec3a7e
eshan-cogoport <eshanpathak999@gmail.com>
2023-12-12 16:09:34 +0530

    long desc changes

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name="uwudu",
     version="1.0.0",
     description="your cli todo list manager (U w U)",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
